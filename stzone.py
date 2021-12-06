aeb4ee38f26f9f4094d40639a0038778ad9e3831
JngMkk <87686562+JngMkk@users.noreply.github.com>
2021-12-06 14:41:30 +0900

    Update stzone.py

--- stzone.py
+++ stzone.py
@@ -23,12 +23,11 @@ def inning():
                 else :
                     sb = 0
                 rows.append([inn, xcd, ycd, sb])
-            time.sleep(5)
     else :
         pass
 
 # 한 경기 모든 이닝 크롤링 + 엑셀 저장
-def game_crawl():
+def inning_crawl():
     columns = ['이닝', '판정', 'x좌표', 'y좌표']
     games = driver.find_elements_by_class_name('search__data')
     for i in range(len(games)):
@@ -36,7 +35,7 @@ def game_crawl():
         time.sleep(10)
         html = driver.page_source
         soup = bs(html, 'html.parser')
-        time.sleep(3)
+        time.sleep(5)
         match = soup.select('div.match__team > span')[0].text + ' ' + soup.select('div.match__team > span')[1].text + ' ' + soup.select('div.match__team > span')[2].text
         date = soup.select_one('div.match__date').text.strip().split('투')[0]
         referee = soup.select_one('div.match__date > span').text.split('(')[1].split('심')[0].split(' ')[0]
@@ -52,7 +51,7 @@ def first_cal(num):
     time.sleep(3)
     for i in range(num):
         driver.find_element_by_class_name('mx-icon-last-month').click()
-        time.sleep(1)
+        time.sleep(3)
         if i == num :
             break
 
@@ -62,22 +61,21 @@ def last_cal(num):
     time.sleep(3)
     for i in range(num):
         driver.find_element_by_class_name('mx-icon-last-month').click()
-        time.sleep(1)
+        time.sleep(3)
         if i == num :
             break
 
 # 한 달 크롤링
-def month_crawl(num):
+def game_crawl(num):
     for i in range(42):
         month = driver.find_element_by_class_name('mx-panel.mx-panel-date')
         day = month.find_elements_by_tag_name('td')[i]
-        time.sleep(3)
         if day.get_attribute('class') == 'cell cur-month' :
             day.click()
-            time.sleep(3)
+            time.sleep(1)
             driver.find_element_by_class_name('mx-datepicker-btn.mx-datepicker-btn-confirm').click()
             time.sleep(10)
-            game_crawl()
+            inning_crawl()
             last_cal(num)
         else :
             continue
@@ -97,12 +95,16 @@ def crawl(num):
 # 2018.11 : 37 ~ 2018.03 : 45
 # 2017.10 : 50 ~ 2017.03 : 57
 
-# driver path 넣기
-driver = webdriver.Chrome(r'your path')
+driver = webdriver.Chrome(r'your path') # path 넣기
 url = 'https://strikes.zone/game/211115DSBKTW'
 driver.get(url)
 time.sleep(10)
-driver.find_element_by_css_selector('div.match__type > button.type__btn.type__btn--inning').click() # 이닝별 데이터 보기
+driver.find_element_by_css_selector('div.match__type > button.type__btn.type__btn--inning').click()
+time.sleep(1)
+driver.find_element_by_css_selector('#__layout > div > div > section > section > header > div.match__type > div > svg').click()
+time.sleep(1)
+driver.find_element_by_css_selector('#__layout > div > div > section > section > header > div.match__type > div > div > ul:nth-child(3) > li:nth-child(1) > button:nth-child(2)').click()
+time.sleep(1)
 crawl_months = [1,2,3,4,5,6,7,8,13,14,15,16,17,18,19,26,27,28,29,30,31,32,33,37,38,39,40,41,42,43,44,45,50,51,52,53,54,55,56,57]
 for i in crawl_months:
     crawl(i)
